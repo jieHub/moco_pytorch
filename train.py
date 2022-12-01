cfda443049bb4a6ca99b8a82ea5de18a542d1552
zhangjie <2434350181@qq.com>
2022-12-01 11:56:45 +0800

    tmp save

---
+++ train.py
@@ -0,0 +1,113 @@
+import argparse
+import builtins
+import math
+import os
+import random
+import shutil
+import time
+import warnings
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.multiprocessing as mp
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+# import loader
+# import builder
+
+model_names = sorted(name for name in models.__dict__ if name.islower() and not name.startswith('__') and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR', help='path to dataset')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50', choices=model_names, help='model architecture: ' 
+        + ' | '.join(model_names) + ' (default: resnet50)')
+parser.add_argument('-j', '--workers', default=32, type=int, metavar='N', help='number of data loadingworkers (default: 32)')
+parser.add_argument('--epochs', default=200, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (luseful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int, metavar='N', 
+        help='mini-batch size (default: 256), this is the total '
+             'batch size of all GPUs on the current node when '
+             'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.03, type=float, metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--shedule', default=[120, 160], nargs='*', type=int, help='learning rate schedule (when to drop lr by 10x)')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum of SGD solver')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)', dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=10, type=int, metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('--world-size', default=-1, type=int, help='number of nodes for distributed training')
+parser.add_argument('--rank', default=-1, type=int, help='node rank for distributed training')
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str, help='url used to set up distributed training')
+psrser.add_argument('--dist-backend', default='nccl', type=str, help='distributed backend')
+parser.add_argument('--seed', default=None, type=int, help='seed for initializing training')
+parser.add_argument('--gpu', default=None, type=int, help='GPU id to use.')
+parser.add_argument('--multiprocessing-distributed', action='store_true', 
+        help='Use multi-processing distributed training to launch '
+             'N processes per node, which has N GPUs. This is the '
+             'fastest way to use PyTorch for either single node or '
+             'multi node data parallel training')
+
+# moco specific configs
+parser.add_argument('--moco-dim', default=128, type=int, help='feature dimension (default: 128)')
+parser.add_argument('--moco-k', default=65536, type=int, help='queue size; number of negative keys (default: 65536)')
+parser.add_argument('--moco-m', default=0.999, type=float, help='moco momentum of updating key encoder (default: 0.999)')
+parser.add_argument('--moco-t', default=0.07, type=float, help='softmax temperature (default: 0.07)')
+
+# options for moco v2
+parser.add_argument('--mlp', action='store_true', help='use mlp head')
+parser.add_argument('--aug-plus', action='store_true', help='use moco v2 data augmentation')
+parser.add_argument('--cos', action='store_true', help='use cosine lr schedule')
+
+
+def main():
+    args = parser.parse_args()
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                  'This will turn on the CUDNN deterministic setting, '
+                  'which can slow down your training considerably! '
+                  'You may see unexpected behavior when restarting '
+                  'from checkpoints.')
+
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                  'disable data parallelism.')
+
+    if args.dist_url == 'env://' and args.world_size == -1:
+        args.world_size = int(os.environ['WORLD_SIZE'])
+
+    args.distributed = args.world_size > 1 or args.multiprocessing_distributed
+
+    ngpus_per_node = torch.cuda.device_count()
+    if args.multiprocessing_distributed:
+        args.world_size = ngpus_per_node * args.world_size
+        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+    else:
+        main_worker(args.gpu, ngpus_per_node, args)
+
+def main_worker(gpu, ngpus_per_node, args):
+    arg.gpu = gpu
+
+    if args.multiprocessing_distributed and args.gpu != 0:
+        def print_pass(*args):
+            pass
+        builtins.print = print_pass
+
+    if args.gpu is not None:
+        print('use gpu: {} for training'.format(args.gpu))
+
+    if 
+
+
+if __name__ == '__main__':
+    main()
